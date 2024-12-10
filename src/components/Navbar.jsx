b784a7dfadb153dc7607983c337471532b352fb0
Kaveengayanga1 <kaveengayanga@gmail.com>
2024-12-11 00:39:04 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import { Navbar as BootstrapNavbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
-export default function Navbar() {
-    return (
-        <div>
-            <ul class="nav justify-content-center">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-        </div>
-    );
-}
+const Navbar = () => {
+  return (
+    <BootstrapNavbar bg="light" expand="lg">
+      <BootstrapNavbar.Brand href="#home">Guide and Grade</BootstrapNavbar.Brand>
+      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+      <BootstrapNavbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#report">Report</Nav.Link>
+          <Nav.Link href="#analyze">Analyze</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+        </Nav>
+      </BootstrapNavbar.Collapse>
+    </BootstrapNavbar>
+  );
+};
+
+export default Navbar;
