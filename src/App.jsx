e611a084903825178c4f7d4029cae4a5b7c1bd75
Kaveengayanga1 <kaveengayanga@gmail.com>
2024-12-26 12:53:47 +0530

    Test deployment

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,9 @@ function App() {
   return (
 
     <div>
-      <Navbar getPageForRouting={pageRouterSetter} />
-      {component}
+      {/* <Navbar getPageForRouting={pageRouterSetter} />
+      {component} */}
+      <h1>Hello There. Test app running here!</h1>
 
 
     </div>
