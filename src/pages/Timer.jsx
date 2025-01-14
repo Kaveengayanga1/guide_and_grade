477a992fd2246eccf150262eebf7229f38003c89
Kaveengayanga1 <kaveengayanga@gmail.com>
2025-01-14 10:38:44 +0530

    Background image added to Timer.

--- src/pages/Timer.jsx
+++ src/pages/Timer.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css'; // Import animate.css for animations
+import image from '../assets/bg.jpg';
 
 const host = "https://guide-and-grade-api.onrender.com";
 //const host = "http://localhost:5000";
-export default function Timer(props){
+export default function Timer(props) {
 
     //Throws error if timer could not be set
     //Sets the timer
     async function setTimer() {
-        let stat=true;
+        let stat = true;
         const requestOptions1 = {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
@@ -19,8 +20,8 @@ export default function Timer(props){
         };
         const setStopPromise = fetch(host + "/setStopTime", requestOptions1).then(
             (response) => {
-                if(!response.ok){
-                    stat=false;
+                if (!response.ok) {
+                    stat = false;
                     throw new Error("Stop time not set successfully");
                 }
             }
@@ -32,10 +33,10 @@ export default function Timer(props){
                 warnTime: warningTime
             }),
         };
-        const setWarnPromise=fetch(host + "/setWarnTime", requestOptions2).then(
+        const setWarnPromise = fetch(host + "/setWarnTime", requestOptions2).then(
             (response) => {
-                if(!response.ok){
-                    stat=false;
+                if (!response.ok) {
+                    stat = false;
                     throw new Error("Warn time not set successfully");
                 }
             }
@@ -47,16 +48,16 @@ export default function Timer(props){
                 startTime: 0
             }),
         };
-        const setStartPromise=fetch(host + "/setStartTime", requestOptions3).then(
+        const setStartPromise = fetch(host + "/setStartTime", requestOptions3).then(
             (response) => {
-                if(!response.ok){
-                    stat=false;
+                if (!response.ok) {
+                    stat = false;
                     throw new Error("Start time not set successfully");
                 }
             }
         );
-        await Promise.all([setStopPromise,setWarnPromise,setStartPromise]);
-        if(!stat){
+        await Promise.all([setStopPromise, setWarnPromise, setStartPromise]);
+        if (!stat) {
             throw new Error("Timer could not be set");
         }
     }
@@ -74,7 +75,7 @@ export default function Timer(props){
             requestOptions
         ).then(
             (response) => {
-                if(!response.ok){
+                if (!response.ok) {
                     throw new Error("Timer could not be reset");
                 }
             }
@@ -90,20 +91,20 @@ export default function Timer(props){
     useEffect(() => {
         AOS.init({ once: true });
         const eventSource = new EventSource(
-          host+"/timer"
+            host + "/timer"
         );
         if (typeof eventSource != undefined) {
-          console.log("Connection with timer successful");
-          eventSource.onmessage = (event) => {
-            const eventData = JSON.parse(event.data);
-            setTime(eventData.time);
-          };
+            console.log("Connection with timer successful");
+            eventSource.onmessage = (event) => {
+                const eventData = JSON.parse(event.data);
+                setTime(eventData.time);
+            };
         } else {
-          console.log("Coudn't connect to timer");
+            console.log("Coudn't connect to timer");
         }
         return () => eventSource.close();
-      }, []);
-    
+    }, []);
+
 
     const formatTime = (seconds) => {
         const mins = Math.floor(time / 60);
@@ -118,21 +119,21 @@ export default function Timer(props){
             headers: { "Content-Type": "application/json" },
             body: "{}",
         };
-        let message= await fetch(
+        let message = await fetch(
             host + "/generateReport",
             requestOptions
         ).then(
             (response) => {
-                if(response.status==204){
+                if (response.status == 204) {
                     return "The microphone was disabled. Please enable the microphone and try again.";
-                }else if(response.status==200){
+                } else if (response.status == 200) {
                     setShowCloseModal(true);
-                }else{
+                } else {
                     return "An error occurred while generating the report";
                 }
             }
         );
-        if(message){
+        if (message) {
             alert(message);
         }
     }
@@ -148,9 +149,9 @@ export default function Timer(props){
                 host + "/stopTimer",
                 requestOptions
             ).then((response) => {
-                if(response.ok){
+                if (response.ok) {
                     setStart(false);
-                }else{
+                } else {
                     alert("Timer could not be stopped");
                 }
             });
@@ -165,9 +166,9 @@ export default function Timer(props){
                 host + "/startTimer",
                 requestOptions
             ).then((response) => {
-                if(response.ok){
+                if (response.ok) {
                     setStart(true);
-                }else{
+                } else {
                     alert("Timer could not be started");
                 }
             });
@@ -196,75 +197,111 @@ export default function Timer(props){
 
     return (
         <div className="d-flex justify-content-center align-items-center vh-100">
-            <div className="container">
-                <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3" data-aos='zoom-in' data-aos-delay="400" data-aos-duration="1000">
-                    <div className="container text-center p-4 shadow rounded bg-light">
-                        <h1 className="mb-4">Timer</h1>
-                        <div
-                            className={`display-4 mb-4 ${time >= warningTime ? 'text-danger animate__animated animate__flash' : ''}`}
-                        >
-                            {formatTime(time)}
-                        </div>
 
-                        {/* Time setter inputs */}
-                        <div className="form-group">
-                            <label htmlFor="totalTime">Total Time (seconds)</label>
-                            <input
-                                type="number"
-                                className="form-control"
-                                id="totalTime"
-                                value={totalTime}
-                                onChange={handleTotalTimeChange}
-                                disabled={start}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="warningTime">Warning Time (seconds)</label>
-                            <input
-                                type="number"
-                                className="form-control"
-                                id="warningTime"
-                                value={warningTime}
-                                onChange={handleWarningTimeChange}
-                                disabled={start}
-                            />
-                        </div>
+            <div className="container-fluid position-relative"
+                style={{
+                    backgroundImage: `url(${image})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    height: "90vh",
+                    width: "100vw",
+                }}
+            >
+                {/* Darker Transparent Overlay */}
+                <div
+                    className="position-absolute top-0 start-0 w-100 h-100"
+                    style={{
+                        backgroundColor: "rgba(0, 0, 0, 0.2)",
+                    }}
+                >
+                </div>
+                <div className="row mt-5"></div>
 
-                        <div className="row mt-3">
-                            <div className="col-12 col-sm-4 mb-2 mb-sm-0">
-                                <button className="btn btn-primary w-100" onClick={handleStartStop}>
-                                    {start ? 'Stop' : 'Start'}
-                                </button>
+                <div className="row mt-5">
+                    <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3"
+                        data-aos='zoom-in'
+                        data-aos-delay="400"
+                        data-aos-duration="1000"
+                    >
+
+                        <div className="container text-center p-4 shadow rounded bg-light">
+                            <h1 className="mb-4">Timer</h1>
+                            <div className={`display-4 mb-4 ${time >= warningTime ? 'text-danger animate__animated animate__flash' : ''}`}                        >
+                                {formatTime(time)}
                             </div>
-                            <div className="col-12 col-sm-4 mb-2 mb-sm-0">
-                                <button className="btn btn-primary w-100" onClick={generateReport} disabled={start}>
-                                    Generate Report
-                                </button>
+
+                            {/* Time setter inputs */}
+                            <div className="form-group">
+                                <label htmlFor="totalTime">Total Time (seconds)</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    id="totalTime"
+                                    value={totalTime}
+                                    onChange={handleTotalTimeChange}
+                                    disabled={start}
+                                />
                             </div>
-                            <div className="col-12 col-sm-4">
-                                <button className="btn btn-danger w-100" onClick={handleReset} disabled={start || time == 0}>
-                                    Reset
-                                </button>
+                            <div className="form-group">
+                                <label htmlFor="warningTime">Warning Time (seconds)</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    id="warningTime"
+                                    value={warningTime}
+                                    onChange={handleWarningTimeChange}
+                                    disabled={start}
+                                />
+                            </div>
+
+                            <div className="row mt-3">
+                                <div className="col-12 col-sm-4 mb-2 mb-sm-0">
+                                    <button className="btn btn-primary w-100"
+                                        onClick={handleStartStop}>
+                                        {start ? 'Stop' : 'Start'}
+                                    </button>
+                                </div>
+                                <div className="col-12 col-sm-4 mb-2 mb-sm-0">
+                                    <button className="btn btn-primary w-100"
+                                        onClick={generateReport} disabled={start}>
+                                        Generate Report
+                                    </button>
+                                </div>
+                                <div className="col-12 col-sm-4">
+                                    <button className="btn btn-danger w-100"
+                                        onClick={handleReset}
+                                        disabled={start || time == 0}>
+                                        Reset
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
 
 
             {/* Modal for Close button clicked */}
-            <div className={`modal fade ${showCloseModal ? 'show d-block' : ''}`} tabIndex="-1" role="dialog" aria-hidden={!showCloseModal}>
+            <div className={`modal fade ${showCloseModal ? 'show d-block' : ''}`}
+                tabIndex="-1" role="dialog"
+                aria-hidden={!showCloseModal}>
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-body">
                             <p>The presentation is being evaluated. Click the button below to view the results.</p>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" onClick={closeCloseModal}>Close</button>
-                            <button type="button" className="btn btn-primary" onClick={() => {
-                                props.setPage('summary');
-                                setShowCloseModal(false);
-                            }}>View Results</button>
+                            <button type="button"
+                                className="btn btn-secondary"
+                                onClick={closeCloseModal}>Close</button>
+                            <button type="button"
+                                className="btn btn-primary"
+                                onClick={() => {
+                                    props.setPage('summary');
+                                    setShowCloseModal(false);
+                                }}>View Results</button>
                         </div>
                     </div>
                 </div>
